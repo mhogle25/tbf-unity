d9d73d855f2f9f2016b0c6ba821e1b22331c7cb4
Mitchell Hogle <mitchellhogle@yahoo.com>
2020-08-11 23:37:45 -0700

    Added job class Added damage function (will deal 1 damage at 0 or less calculated damage)

--- TurnBasedEngine/Assets/Scripts/Player/PlayerStats.cs
+++ TurnBasedEngine/Assets/Scripts/Player/PlayerStats.cs
@@ -1,6 +1,10 @@
-﻿
-
-public class PlayerStats : Stats
+﻿public class PlayerStats : Stats
 {
-    
+    public Job Profession { get { return _profession; } }
+    private Job _profession;
+    public int Experience { get { return _experience; } }
+    private int _experience;
+    public int Level { get { return _level; } }
+    private int _level;
+
 }
