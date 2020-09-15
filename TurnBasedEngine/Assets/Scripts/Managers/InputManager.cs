40519cd17622f1df74bd7040c4a5cd58ccd2eeeb
MITCHLAPTOP\mitch <mhogle@ehuman.com>
2020-09-14 19:40:47 -0700

    Some small changes

--- TurnBasedEngine/Assets/Scripts/Managers/InputManager.cs
+++ TurnBasedEngine/Assets/Scripts/Managers/InputManager.cs
@@ -3,20 +3,32 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public static bool IsGamepadConnected { get; private set; } = false;
-    public static float HorizontalAxis { get; private set; } = 0;
-    public static float VerticalAxis { get; private set; } = 0;
-    public static bool Up { get; private set; } = false;
-    public static bool Left { get; private set; } = false;
-    public static bool Down { get; private set; } = false;
-    public static bool Right { get; private set; } = false;
-    public static bool Confirm { get; private set; } = false;
-    public static bool Back { get; private set; } = false;
-    public static bool Menu { get; private set; } = false;
-    public static bool Attack { get; private set; } = false;
-    public static bool Pause { get; private set; } = false;
-    public static bool Select { get; private set; } = false;
-
+    public static bool IsGamepadConnected { get { return _isGamepadConnected; } }
+    private static bool _isGamepadConnected = false;
+    public static float HorizontalAxis { get { return _horizontalAxis; } }
+    private static float _horizontalAxis = 0;
+    public static float VerticalAxis { get { return _verticalAxis; } }
+    private static float _verticalAxis = 0;
+    public static bool Up { get { return _up; } }
+    private static bool _up = false;
+    public static bool Left { get { return _down; } }
+    private static bool _down = false;
+    public static bool Down { get { return _left; } }
+    private static bool _left = false;
+    public static bool Right { get { return _right; } }
+    private static bool _right = false;
+    public static bool Confirm { get { return _confirm; } }
+    private static bool _confirm = false;
+    public static bool Back { get { return _back; } }
+    private static bool _back = false;
+    public static bool Menu { get { return _menu; } }
+    private static bool _menu = false;
+    public static bool Attack { get { return _attack; } }
+    private static bool _attack = false;
+    public static bool Pause { get { return _pause; } }
+    private static bool _pause = false;
+    public static bool Select { get { return _select; } }
+    private static bool _select = false;
 
     private Action inputListener;
     private float joystickThreshold = 0.5f;
@@ -37,68 +49,68 @@ public class InputManager : MonoBehaviour
 
     private void Gamepad() {
         //Set Key Press Flags
-        Confirm = Input.GetKey(ControlsConfig.GamepadConfirm);
-        Back = Input.GetKey(ControlsConfig.GamepadBack);
-        Menu = Input.GetKey(ControlsConfig.GamepadMenu);
-        Attack = Input.GetKey(ControlsConfig.GamepadAttack);
-        Pause = Input.GetKey(ControlsConfig.GamepadPause);
-        Select = Input.GetKey(ControlsConfig.GamepadSelect);
+        _confirm = Input.GetKey(ControlsConfig.GamepadConfirm);
+        _back = Input.GetKey(ControlsConfig.GamepadBack);
+        _menu = Input.GetKey(ControlsConfig.GamepadMenu);
+        _attack = Input.GetKey(ControlsConfig.GamepadAttack);
+        _pause = Input.GetKey(ControlsConfig.GamepadPause);
+        _select = Input.GetKey(ControlsConfig.GamepadSelect);
 
         //Set Horizontal and Vertical Axis Values
-        HorizontalAxis = Input.GetAxis("Horizontal");
-        VerticalAxis = Input.GetAxis("Vertical");
+        _horizontalAxis = Input.GetAxis("Horizontal");
+        _verticalAxis = Input.GetAxis("Vertical");
 
         //Set Directional Press flags
-        if (HorizontalAxis > joystickThreshold) {
-            Right = true;
-        } else if (HorizontalAxis < -joystickThreshold) {
-            Left = true;
+        if (_horizontalAxis > joystickThreshold) {
+            _right = true;
+        } else if (_horizontalAxis < -joystickThreshold) {
+            _down = true;
         } else {
-            Left = false;
-            Right = false;
+            _down = false;
+            _right = false;
         }
 
-        if (VerticalAxis > joystickThreshold) {
-            Up = true;
-        } else if (VerticalAxis < -joystickThreshold) {
-            Down = true;
+        if (_verticalAxis > joystickThreshold) {
+            _up = true;
+        } else if (_verticalAxis < -joystickThreshold) {
+            _left = true;
         } else {
-            Up = false;
-            Down = false;
+            _up = false;
+            _left = false;
         }
     }
 
     private void Keyboard() {
         //Set key press flags
-        Up = Input.GetKey(ControlsConfig.KeyboardUp);
-        Left = Input.GetKey(ControlsConfig.KeyboardLeft);
-        Down = Input.GetKey(ControlsConfig.KeyboardDown);
-        Right = Input.GetKey(ControlsConfig.KeyboardRight);
-        Confirm = Input.GetKey(ControlsConfig.KeyboardConfirm);
-        Back = Input.GetKey(ControlsConfig.KeyboardBack);
-        Menu = Input.GetKey(ControlsConfig.KeyboardMenu);
-        Attack = Input.GetKey(ControlsConfig.KeyboardAttack);
-        Pause = Input.GetKey(ControlsConfig.KeyboardPause);
-        Select = Input.GetKey(ControlsConfig.KeyboardSelect);
+        _up = Input.GetKey(ControlsConfig.KeyboardUp);
+        _down = Input.GetKey(ControlsConfig.KeyboardLeft);
+        _left = Input.GetKey(ControlsConfig.KeyboardDown);
+        _right = Input.GetKey(ControlsConfig.KeyboardRight);
+        _confirm = Input.GetKey(ControlsConfig.KeyboardConfirm);
+        _back = Input.GetKey(ControlsConfig.KeyboardBack);
+        _menu = Input.GetKey(ControlsConfig.KeyboardMenu);
+        _attack = Input.GetKey(ControlsConfig.KeyboardAttack);
+        _pause = Input.GetKey(ControlsConfig.KeyboardPause);
+        _select = Input.GetKey(ControlsConfig.KeyboardSelect);
 
         //Set Horizontal and Vertical Axis Values
         float tempHor;
         float tempVer;
-        if (Left && Right) {
+        if (_down && _right) {
             tempHor = 0;
-        } else if (Left) {
+        } else if (_down) {
             tempHor = -1;
-        } else if (Right) {
+        } else if (_right) {
             tempHor = 1;
         } else {
             tempHor = 0;
         }
 
-        if (Up && Down) {
+        if (_up && _left) {
             tempVer = 0;
-        } else if (Down) {
+        } else if (_left) {
             tempVer = -1;
-        } else if (Up) {
+        } else if (_up) {
             tempVer = 1;
         } else {
             tempVer = 0;
@@ -110,8 +122,8 @@ public class InputManager : MonoBehaviour
             tempVer *= (Mathf.Sqrt(2) / 2);
         }
 
-        HorizontalAxis = tempHor;
-        VerticalAxis = tempVer;
+        _horizontalAxis = tempHor;
+        _verticalAxis = tempVer;
     }
 
     private void GamepadConnected() {
@@ -119,14 +131,14 @@ public class InputManager : MonoBehaviour
         string[] gamepadNames = Input.GetJoystickNames();
         if (gamepadNames.Length > 0) {
             if (gamepadNames[0] != string.Empty) {
-                if (!IsGamepadConnected) {
-                    IsGamepadConnected = true;
+                if (!_isGamepadConnected) {
+                    _isGamepadConnected = true;
                     inputListener += Gamepad;
                     inputListener -= Keyboard;
                 }
             } else {
-                if (IsGamepadConnected) {
-                    IsGamepadConnected = false;
+                if (_isGamepadConnected) {
+                    _isGamepadConnected = false;
                     inputListener -= Gamepad;
                     inputListener += Keyboard;
                 }
--- TurnBasedEngine/Assets/Scripts/Utilities/Stats.cs
+++ TurnBasedEngine/Assets/Scripts/Utilities/Stats.cs
@@ -2,31 +2,31 @@
 public class Stats
 {
     public int Health { get { return _health; } }
-    private int _health;
+    private protected int _health;
     public int MaxHealth { get { return _maxHealth; } }
-    private int _maxHealth;
+    private protected int _maxHealth;
     public int Stamina { get { return _stamina; } }
-    private int _stamina;
+    private protected int _stamina;
     public int MaxStamina { get { return _maxStamina; } }
-    private int _maxStamina;
+    private protected int _maxStamina;
     public int Speed { get { return _speed; } }
-    private int _speed;
+    private protected int _speed;
     public int Swiftness { get { return _swiftness; } }
-    private int _swiftness;
+    private protected int _swiftness;
     public int Attack { get { return _attack + _strength; } }
-    private int _attack;
+    private protected int _attack;
     public int Strength { get { return _strength; } }
-    private int _strength;
+    private protected int _strength;
     public int Defense { get { return _defense + _toughness; } }
-    private int _defense;
+    private protected int _defense;
     public int Toughness { get { return _toughness; } }
-    private int _toughness;
+    private protected int _toughness;
     public int Focus { get { return _focus + _will; } }
-    private int _focus;
+    private protected int _focus;
     public int Will { get { return _will; } }
-    private int _will;
+    private protected int _will;
     public int Luck { get { return _luck; } }
-    private int _luck;
+    private protected int _luck;
 
     public void Damage(int damage) {
         _health -= (damage - Defense) > 0 ? (damage - Defense) : 1;
